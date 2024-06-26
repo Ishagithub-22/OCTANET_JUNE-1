1e71439b760f728768731da7c34dd3865a630c4f
Ishagithub-22 <mansinghisha@gmail.com>
2024-06-26 22:44:15 +0530

    Add files via upload

---
+++ LandingPage-script.js
@@ -0,0 +1,13 @@
+
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+});
+
+
+document.getElementById("reviewForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let review = document.getElementById("review").value;
+   
+});
